7c0b0d7ddfc995752f2e59e58e3fd4395b5e8ca2
Ty Sanders <tysanders@Tys-MacBook-Air.local>
2022-05-14 13:58:51 -0500

    API

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
-	"encoding/json"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
@@ -22,15 +25,66 @@ type Director struct {
 
 var movies []Movie
 
-getMovies(w http.ResponseWriter, router *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
+func getMovies(writer http.ResponseWriter, router *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(movies)
+}
+
+func getMovie(writer http.ResponseWriter, router *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(router)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(writer).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(writer http.ResponseWriter, router *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(router.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000))
+	movies = append(movies, movie)
+	json.NewEncoder(writer).Encode(movies)
+}
+
+func updateMovie(writer http.ResponseWriter, router *http.Request) {
+	//set json content type
+	writer.Header().Set("Content-Type", "application/json")
+	//json params
+	params := mux.Vars(router)
+	//loop thru movies,range
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(router.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(writer).Encode(movie)
+		}
+	}
+}
+
+func deleteMovie(writer http.ResponseWriter, router *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(router)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1])
+			break
+		}
+	}
+	json.NewEncoder(writer).Encode(movies)
 }
 
 func main() {
 	router := mux.NewRouter()
 
 	movies = append(movies, Movie{ID: "1", Isbn: "123", Title: "Free Guy", Director: &Director{Firstname: "John", Lastname: "Wick"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "124", Title: "Adam Project", Director: &Director{Firstname: "Ryan", Lastname: "Reynolds"}})
 	router.HandleFunc("/movies", getMovies).Methods("GET")
 	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	router.HandleFunc("/movies", createMovie).Methods("POST")
