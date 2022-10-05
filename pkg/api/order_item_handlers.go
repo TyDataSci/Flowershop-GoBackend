566e93d97001c7c47146b27707b67394ba0875c1
TyDataSci <sandetab2@gmail.com>
2022-10-05 15:56:07 -0500

    CORS handling Order Item Routes

--- pkg/api/order_item_handlers.go
+++ pkg/api/order_item_handlers.go
@@ -12,7 +12,10 @@ import (
 
 func GetOrderItems(writer http.ResponseWriter, router *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
-	writer.Header().Set("Access-Control-Allow-Origin", "*")
+	writer.Header().Set("Access-Control-Allow-Origin", "http://foreveryoursflowershop.com")
+	writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+	writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie")
 	params := mux.Vars(router)
 	orderid, _ := strconv.Atoi(params["orderid"])
 	order_items, _ := db.GetOrderItems(orderid)
@@ -21,7 +24,10 @@ func GetOrderItems(writer http.ResponseWriter, router *http.Request) {
 
 func CreateOrderItem(writer http.ResponseWriter, router *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
-	writer.Header().Set("Access-Control-Allow-Origin", "*")
+	writer.Header().Set("Access-Control-Allow-Origin", "http://foreveryoursflowershop.com")
+	writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+	writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie")
 	params := mux.Vars(router)
 	paramsOrderID, _ := strconv.Atoi(params["orderid"])
 	paramsItemID, _ := strconv.Atoi(params["itemid"])
@@ -32,7 +38,10 @@ func CreateOrderItem(writer http.ResponseWriter, router *http.Request) {
 
 func RemoveOrderItem(writer http.ResponseWriter, router *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
-	writer.Header().Set("Access-Control-Allow-Origin", "*")
+	writer.Header().Set("Access-Control-Allow-Origin", "http://foreveryoursflowershop.com")
+	writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+	writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie")
 	params := mux.Vars(router)
 	paramsOrderID, _ := strconv.Atoi(params["orderid"])
 	paramsItemID, _ := strconv.Atoi(params["itemid"])
